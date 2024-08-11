2c2a7cc2463ae14b8961f216d7fc3b98d8e117bf
zaticpetru <zatic.petru@outlook.com>
2024-08-11 15:47:01 +0300

    Fix click events for timeline

--- src/App.tsx
+++ src/App.tsx
@@ -36,22 +36,26 @@ function App() {
   });
 
   return (
-    <MapContainer
-      center={INITIAL_MAP_CENTER}
-      bounds={INITIAL_MAP_BOUNDARIES_INSTANCE}
-      zoom={INITIAL_MAP_ZOOM}
-      scrollWheelZoom
-      style={{ height: "100dvh", width: "100dvw" }}
-    >
-      <TileLayer
-        attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a><strong> <br/> <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
-        url={isSatelliteMode ? tileLayers["satellite"] : tileLayers["streets"]}
-      ></TileLayer>
-      <OverlayItems />
-      {isLoading ? <LoadingWheel /> : <Space events={events as Event[]} />}
-      <EventDetailsModal open={!!eventId} />
+    <>
+      <MapContainer
+        center={INITIAL_MAP_CENTER}
+        bounds={INITIAL_MAP_BOUNDARIES_INSTANCE}
+        zoom={INITIAL_MAP_ZOOM}
+        scrollWheelZoom
+        style={{ height: "100dvh", width: "100dvw" }}
+      >
+        <TileLayer
+          attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a><strong> <br/> <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+          url={
+            isSatelliteMode ? tileLayers["satellite"] : tileLayers["streets"]
+          }
+        ></TileLayer>
+        <OverlayItems />
+        {isLoading ? <LoadingWheel /> : <Space events={events as Event[]} />}
+        <EventDetailsModal open={!!eventId} />
+      </MapContainer>
       <Time events={events as Event[]} />
-    </MapContainer>
+    </>
   );
 }
 
