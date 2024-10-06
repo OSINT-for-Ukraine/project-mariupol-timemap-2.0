3bdde00af8b1140aad66ab35f8ac20b842f09bfe
zaticpetru <zatic.petru@outlook.com>
2024-10-06 15:45:42 +0300

    Added debug message for env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import generateFile from "vite-plugin-generate-file";
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [
-    react(),
-    generateFile([
-      {
-        output: "./_redirects",
-        type: "template",
-        template: "./template.ejs",
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  console.log(`process.env.VITE_TEST = ${process.env.VITE_TEST}`);
+
+  return {
+    plugins: [
+      react(),
+      generateFile([
+        {
+          output: "./_redirects",
+          type: "template",
+          template: "./template.ejs",
+        },
+      ]),
+    ],
+    resolve: {
+      alias: {
+        Components: path.resolve(__dirname, "./src/Components"),
+        assets: path.resolve(__dirname, "./src/assets"),
+        providers: path.resolve(__dirname, "./src/providers"),
+        utils: path.resolve(__dirname, "./src/utils"),
       },
-    ]),
-  ],
-  resolve: {
-    alias: {
-      Components: path.resolve(__dirname, "./src/Components"),
-      assets: path.resolve(__dirname, "./src/assets"),
-      providers: path.resolve(__dirname, "./src/providers"),
-      utils: path.resolve(__dirname, "./src/utils"),
     },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
     },
-  },
+  };
 });
